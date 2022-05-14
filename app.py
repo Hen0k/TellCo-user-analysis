d7c395576ad98ec38bfb0245924b0017fc2e62e7
Hen0k <henoktilaye7@gmail.com>
2022-05-14 23:24:15 +0300

    Show users score table

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from mysql_manager import DBManager
+
+
+st.set_page_config(layout="wide")
+
+st.cache()
+
+
+def load_data():
+    db_manager = DBManager()
+    db_manager.setup()
+    df = db_manager.get_tellco_users_data()
+    return df
+
+
+users_data = load_data()
+
+st.title('TellCo Users Analysis')
+
+st.write("User MSISDN with various scores")
+
+st.dataframe(users_data)
